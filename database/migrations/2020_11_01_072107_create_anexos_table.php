51cf4cf9ef06239dcfbf1788c219999958e87e1f
Adriano Christian <adrianochristian3@gmail.com>
2020-11-01 04:44:58 -0300

    Start nas migrations

---
+++ database/migrations/2020_11_01_072107_create_anexos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnexosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anexos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_paciente');
+            $table->foreign('id_paciente')->references('id')->on('pacientes');
+            $table->string('titulo');
+            $table->string('arquivo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('anexos');
+    }
+}
---
+++ database/migrations/2020_11_01_074244_create_agendamentos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAgendamentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agendamentos', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('data_consulta');
+            $table->string('horario');
+            $table->unsignedBigInteger('id_paciente');
+            $table->foreign('id_paciente')->references('id')->on('pacientes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agendamentos');
+    }
+}
